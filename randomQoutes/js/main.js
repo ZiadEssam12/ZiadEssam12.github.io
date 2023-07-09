b4e06041d71a6d35104847e50921951885b1629d
ZiadEssam12 <43687395+ZiadEssam12@users.noreply.github.com>
2023-07-09 06:57:49 +0300

    Add files via upload

---
+++ randomQoutes/js/main.js
@@ -0,0 +1,53 @@
+var qoute = document.getElementById("quote");
+var author = document.getElementById("author");
+var qoutes=[
+    {
+        qoute:"Be yourself; everyone else is already taken." ,
+        author:"Oscar Wilde"
+    },
+    {
+        qoute:"A friend is someone who knows all about you and still loves you." ,
+        author:"Elbert Hubbard"
+    },
+    {
+        qoute:"So many books, so little time." ,
+        author:"Frank Zappa"
+    },
+    {
+        qoute:"A room without books is like a body without a soul." ,
+        author:"Marcus Tullius Cicero"
+    },
+    {
+        qoute:"You only live once, but if you do it right, once is enough." ,
+        author:"Mae West"
+    },
+];
+
+// document.getElementById("qouteDiv").style.visibility = "hidden";
+var oldrandom = -99;
+
+function qouteGen(){
+    document.getElementById("qouteDiv").style.visibility = "unset";
+    document.getElementById("button").innerHTML="New Qoute";
+    randomNumebr = returnRandom();
+
+
+    while (true){
+        randomNumebr = returnRandom();
+        if (oldrandom != randomNumebr){
+            break;
+        }
+    }
+
+    qoute.innerHTML=` ${qoutes[randomNumebr]["qoute"]} `;
+    author.innerHTML=` ${qoutes[randomNumebr]["author"]}  `;
+    oldrandom =randomNumebr;
+    
+}
+
+
+function returnRandom(){
+    var randomNumber= Math.random() * (qoutes.length-1);
+    randomNumber = Math.round(randomNumber);
+    return randomNumber
+}
